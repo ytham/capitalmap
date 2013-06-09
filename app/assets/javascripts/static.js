639980dd2cd43229c449c169a2bbe0250ac58b4e
mike-tang <michael.c.tang@gmail.com>
2013-06-09 12:54:57 -0700

    Chevron sidebar

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
 
 	var url = window.location.pathname;
 	var filename = url.match(/.*\/(.*)$/)[1];
-	console.log(filename);
-	//$(document).getElementById(sidebarId).append();
+	$(document).getElementById(filename).append('<i class="icon-chevron-right icon-white"></i>');
 })
