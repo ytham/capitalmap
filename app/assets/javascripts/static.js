5ab088f2e98aec44ef909317d0780611dbd0d6b5
mike-tang <michael.c.tang@gmail.com>
2013-06-09 12:30:01 -0700

    update static.js

---
+++ app/assets/javascripts/static.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	var url = window.location.pathname;
+	var filename = url.match(/.*\/(.*)$/)[1];
+	console.log(filename);
+	//$(document).getElementById(sidebarId).append();
+})
