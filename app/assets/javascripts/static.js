12ecd1c0b236e5679dcc815fc83be32990287383
mike-tang <michael.c.tang@gmail.com>
2013-06-09 14:53:13 -0700

    navbar padding

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 	var filename = url.match(/.*\/(.*)$/)[1];
 	var icon = '<i class="icon-chevron-right icon-white"></i>';
 	document.getElementById(filename).innerHTML += icon;
-	$('#'+filename).parent(".accordion-body").addClass("in");
+	$('#leading_indicators').parent().parent().parent().parent().find(".accordion-body").addClass("in");
 
 })
